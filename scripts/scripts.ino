4f6de8197a0ef023cc778fae4360819c6a1f1556
NotHydra <rizky.irswanda115@gmail.com>
2024-03-30 17:06:16 +0800

    feat: led implementation

--- scripts/scripts.ino
+++ scripts/scripts.ino
@@ -6,11 +6,15 @@ int numMeasure = 128;
 
 int soundPin = A0;
 
+int ledRedPin = 9;
+int ledYellowPin = 10;
+int ledGreenPin = 11;
+
 long sum = 0;
 long level = 0;
 
-int soundLow = 40;
-int soundmedium = 400;
+int soundLow = 125;
+int soundmedium = 350;
 
 void setup()
 {
@@ -20,6 +24,10 @@ void setup()
   lcd.backlight();
 
   pinMode(soundPin, INPUT);
+
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledYellowPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
 }
 
 void loop()
@@ -40,17 +48,29 @@ void loop()
   if (level < soundLow)
   {
     lcd.print("Level=Low");
+
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledYellowPin, LOW);
+    digitalWrite(ledGreenPin, HIGH);
   }
   else if (level > soundLow && level < soundmedium)
   {
     lcd.print("Level=Medium");
+
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledYellowPin, HIGH);
+    digitalWrite(ledGreenPin, LOW);
   }
   else if (level > soundmedium)
   {
     lcd.print("Level=High");
+
+    digitalWrite(ledRedPin, HIGH);
+    digitalWrite(ledYellowPin, LOW);
+    digitalWrite(ledGreenPin, LOW);
   }
 
-  delay(500);
+  delay(100);
 
   sum = 0;
   lcd.clear();
